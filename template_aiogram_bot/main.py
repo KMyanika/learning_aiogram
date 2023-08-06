e656c738a0d6c0ed85fb943c3b35f920add53b5e
Yanika <yanikam22@gmail.com>
2023-08-06 12:15:48 +0500

    Модифицировали файл main.py: - Создали команды help для связи пользователя с админом - Обработали запрос help чтобы сообщение уходило админу - Заменили текстоые сообщения на import из text_handlers

--- template_aiogram_bot/main.py
+++ template_aiogram_bot/main.py
@@ -4,6 +4,7 @@ import dotenv
 
 import asyncio
 from default_commands import *
+from text_handlers import *
 
 dotenv.load_dotenv()
 token = os.getenv("API_TOKEN")
@@ -12,6 +13,8 @@ admin = int(os.getenv("ADMIN"))
 bot = Bot(token)
 dp = Dispatcher(bot)
 pm = 'Markdown'
+handlers_help_command = {}
+
 
 @dp.message_handler(commands=["start"])
 async def command_start(message: types.Message):
@@ -22,19 +25,45 @@ async def command_start(message: types.Message):
         await bot.send_message(chat_id=admin,
                                text=f'#newuser: @{message.from_user.username}\n'
                                     f'ID: `{message.chat.id}`\n'
-                                    f'[Написать сообщение](tg://user?id={message.chat.id})',
+                                    f'[Написать сообщение](tg://user?id={message.chat.id})',     #todo спрятать текстовые сообщения в text_handlers
                                parse_mode=pm)
 
 
-@dp.message_handler(commands=["getmyid"])
+@dp.message_handler(commands=["getmyid"])  #todo на следующий урок попробовать добавить кнопку переслать сообщение
 async def command_getmyid(message: types.Message):
     await message.answer(text=f"Ваш ID: `{message.chat.id}`",
                          parse_mode=pm)
 
 
+@dp.message_handler(commands=["help"])
+async def command_help(message: types.Message):
+    await message.answer(text=ForUsers.push_command_help(message))
+    handlers_help_command[message.chat.id] = 'H'
+
+
+
+
 @dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
+async def messages_handlers(message: types.Message):
+    message.text = message.text.lower().strip()
+    if message.text == 'контакты':
+        pass  #todo создать сообщение с моими контактами
+
+    elif any(x in message.text for x in ('#help', 'help', 'помощь')):
+        await bot.send_message(chat_id=admin,
+                               text=ForAdmin.push_help_handlers(message, message.text),
+                               parse_mode=pm)
+        await message.answer(text=ForUsers.push_help_handlers(message))
+
+    else:
+        if handlers_help_command[message.chat.id] == 'H':
+            handlers_help_command[message.chat.id] = ''
+            await bot.send_message(chat_id=admin,
+                                   text=ForAdmin.push_help_handlers(message, message.text),
+                                   parse_mode=pm)
+            await message.answer(text=ForUsers.push_help_handlers(message))
+
+
 
 
 
